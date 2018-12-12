0bade3c04c4442175a00b7c6d954cdb9da5da438
田中宏法 <ironotnka0105@gmail.com>
2018-12-13 08:49:14 +0900

    events indexを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'blogs#index'
   resources :blogs
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
