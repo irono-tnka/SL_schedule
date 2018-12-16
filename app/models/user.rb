a0d98f78a3adb79f6177f2386d8c1e59e6bd3960
田中宏法 <ironotnka0105@gmail.com>
2018-12-16 17:22:47 +0900

    user 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
 
-    enum position:{"left":0, "center":1, "right":2, "setter":3, "libero":4} # position
+    enum position:{"left": 0, "center": 1, "right": 2, "setter": 3, "libero": 4} # position
 end
