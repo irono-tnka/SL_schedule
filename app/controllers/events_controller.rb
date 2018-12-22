92bb904640b8ceacb01a6c9680adb4bb77b0d923
田中宏法 <ironotnka0105@gmail.com>
2018-12-22 18:57:29 +0900

    event radio_button 追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,60 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def index
-    @users = User.all
+    @events = Event.all
   end
 
   def show
   end
 
   def new
-    @event = Eent.new
+    @event = Event.new
   end
 
   def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    raise
+    
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.require(:event).permit(:user_id, :date, :start_time, :end_time, :location, :memo)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
-    belongs_to :user
+    # belongs_to :user
+    enum event_type:{practice: 0, tournament: 1} 
 end
