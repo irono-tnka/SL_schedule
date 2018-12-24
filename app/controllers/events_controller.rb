1dbc0b046b9c787b852b6b476929b5d9e8dad853
田中宏法 <ironotnka0105@gmail.com>
2018-12-24 23:50:03 +0900

    event 時間設定　初期のデフォルト追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,9 +17,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-
-  
-
+    
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
